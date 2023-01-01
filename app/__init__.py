efbeeb45ebbdd3a4a2ab3526bb5480593902f0e5
Ugochukwu Nwachukwu <danielnwachukwu5738@gmail.com>
2023-01-01 09:17:12 +0100

    First commit

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,15 @@ from flask import Flask
 from config import Config
 from app.extensions import db
 
+import logging
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    # if app.config['LOG_WITH_GUNICORN']:
+    #     gunicorn_error_logger = logging.getLogger('gunicorn.error')
+    #     app.logger.handlers.extend(gunicorn_error_logger.handlers)
+    #     app.logger.setLevel(logging.DEBUG)
 
     # Initialize Flask extensions
     db.init_app(app)
